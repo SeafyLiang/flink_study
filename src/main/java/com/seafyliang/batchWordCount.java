fb07784b2d098653859130af28c0c09ae921c973
SeafyLiang <seafyliang@icloud.com>
2020-12-24 14:37:01 +0800

    Initial commit

---
+++ src/main/java/com/seafyliang/batchWordCount.java
@@ -0,0 +1,17 @@
+/**
+ * FileName: batchWordCount
+ * Author:   SeafyLiang
+ * Date:     2020/12/24 下午2:35
+ * Description: 批处理wordcount
+ */
+package com.seafyliang;
+
+/**
+ * 〈批处理wordcount〉
+ *
+ * @author SeafyLiang
+ * @create 2020/12/24
+ * @since 1.0.0
+ */
+public class batchWordCount {
+}
